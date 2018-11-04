8f80b7e13a8604d4453bfa65b4b464135d8c4d7e
bdelate <brendon.delate@gmail.com>
2018-11-04 09:23:46 +0200

    Implemented process directories, directory and files in run.py along with tests. Upload_logs partially implemented, to be completed when backend api has been created.

--- src/run.py
+++ src/run.py
@@ -2,27 +2,76 @@
 import logging
 import pathlib
 
+# 3rd party imports
+from sqlalchemy.orm.session import Session
+
 # project imports
+import model
 import settings
 
 # Setup logging to file
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
-file_handler = logging.FileHandler('logitall.log')
-formatter = logging.Formatter('%(asctime)s: %(message)s')
+file_handler = logging.FileHandler("logitall.log")
+formatter = logging.Formatter("%(asctime)s: %(message)s")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 
-def db_exists() -> bool:
-    """Check if the monitoring db exists"""
-    path = pathlib.Path(settings.DB_LOCATION)
+def process_directories(session: Session) -> None:
+    """Call process_directory for each directory in the database"""
+    directories = session.query(model.Directory).all()
+    for directory in directories:
+        process_directory(directory=directory, session=session)
+
+
+def process_directory(directory: model.Directory, session: Session) -> None:
+    """Call process_file for each file in the specified directory"""
+    path = pathlib.Path(directory.path)
     if path.exists():
-        return True
-    logger.error('Monitoring db does not exist. Please run configure.py')
-    return False
+        for file in path.glob(pattern="*"):
+            if file.is_file():
+                process_file(directory=directory, file=file, session=session)
+    else:
+        logger.error(f"Invalid directory: {path}")
 
 
-if __name__ == '__main__':
-    if db_exists():
-        print('database exists')
+def process_file(
+    directory: model.Directory, file: pathlib.Path, session: Session
+) -> None:
+    """
+    If the file doesn't exist in the db, upload it and add it to the db.
+    If the file exists in the db, upload any new content and update its size.
+    """
+    db_file = (
+        session.query(model.File)
+        .filter_by(directory_id=directory.id, name=file.name)
+        .first()
+    )
+    if db_file is None:
+        upload_logs(file=file, start_pos=0)
+        new_db_file = model.File(
+            name=file.name, size=file.stat().st_size, directory_id=directory.id
+        )
+        session.add(new_db_file)
+        session.commit()
+    elif file.stat().st_size > db_file.size:
+        upload_logs(file=file, start_pos=db_file.size)
+        db_file.size = file.stat().st_size
+        session.add(db_file)
+        session.commit()
+
+
+def upload_logs(file: pathlib.Path, start_pos: int) -> None:
+    """Upload file to the server from start_pos to the end of the file"""
+    with open(file) as open_file:
+        open_file.seek(start_pos)
+        for line in open_file:
+            entry = line.rstrip("\n")
+            print(f"to upload: {entry}")
+
+
+if __name__ == "__main__":
+    session = model.get_db_session()
+    process_directories(session=session)
+    session.close()
--- src/tests/test_run.py
+++ src/tests/test_run.py
@@ -1,15 +1,135 @@
 # stdlib imports
+import pathlib
 import unittest
 from unittest import mock
 
+# 3rd party imports
+from pyfakefs import fake_filesystem_unittest
+
 # project imports
+import model
 import run
+import settings
 
 
-class TestStartUp(unittest.TestCase):
-    @mock.patch('run.logger')
-    def test_log_error_if_no_db(self, mock_logger):
-        """Test that an error log is created if db does not exist"""
-        db_exists = run.db_exists()
-        self.assertTrue(mock_logger.error.called)
-        self.assertFalse(db_exists)
+class Run(fake_filesystem_unittest.TestCase):
+    def setUp(self):
+        settings.DB_LOCATION = "sqlite:///:memory:"
+        self.setUpPyfakefs()
+        self.session = model.get_db_session()
+
+        self.path_1 = "/home"
+        self.directory_1 = model.Directory(path=self.path_1)
+        self.session.add(self.directory_1)
+
+        self.path_2 = "/var"
+        self.directory_2 = model.Directory(path=self.path_2)
+        self.session.add(self.directory_2)
+        self.session.commit()
+
+        self.file_1_name = "file_1"
+        self.file_1_content = "test"
+        self.fake_file_1 = self.fs.create_file(
+            f"{self.path_1}/{self.file_1_name}", contents=self.file_1_content
+        )
+
+        self.db_file_1 = model.File(
+            name=self.file_1_name,
+            size=len(self.file_1_content),
+            directory_id=self.directory_1.id,
+        )
+        self.session.add(self.db_file_1)
+
+        self.file_2_name = "file_2"
+        self.file_2_content = "test"
+        self.fake_file_2 = self.fs.create_file(
+            f"{self.path_1}/{self.file_2_name}"
+        )
+        self.db_file_2 = model.File(
+            name=self.file_2_name,
+            size=len(self.file_2_content),
+            directory_id=self.directory_1.id,
+        )
+
+        self.session.commit()
+
+    def tearDown(self):
+        self.session.close()
+
+    @mock.patch("run.process_directory")
+    def test_process_directories(self, mock_process_directory):
+        """Test that process_directory is called correctly for every directory"""
+        run.process_directories(session=self.session)
+        self.assertEqual(mock_process_directory.call_count, 2)
+        mock_process_directory.assert_called_with(
+            directory=self.directory_2, session=self.session
+        )
+
+    @mock.patch("run.process_file")
+    def test_process_directory(self, mock_process_file):
+        """Test that process file is called for each file in a directory"""
+        run.process_directory(directory=self.directory_1, session=self.session)
+        self.assertEqual(mock_process_file.call_count, 2)
+        path = pathlib.Path(self.fake_file_2.path)
+        mock_process_file.assert_called_with(
+            directory=self.directory_1, file=path, session=self.session
+        )
+
+    @mock.patch("run.logger")
+    @mock.patch("run.process_file")
+    def test_process_invalid_directory(self, mock_process_file, mock_logger):
+        """Test that an error is logged if an invalid directory is used"""
+        with mock.patch("pathlib.Path.exists", return_value=False):
+            run.process_directory(
+                directory=self.directory_1, session=self.session
+            )
+            self.assertEqual(mock_process_file.call_count, 0)
+            self.assertTrue(mock_logger.error.called)
+
+    @mock.patch("run.upload_logs")
+    def test_process_existing_file_unchanged(self, mock_upload_logs):
+        """An existing unchanged file requires no action"""
+        file = pathlib.Path(f"{self.path_1}/{self.file_1_name}")
+        run.process_file(
+            directory=self.directory_1, file=file, session=self.session
+        )
+        self.assertTrue(mock_upload_logs.not_called)
+        db_file = self.session.query(model.File).first()
+        self.assertEqual(db_file.size, len(self.file_1_content))
+
+    @mock.patch("run.upload_logs")
+    def test_process_existing_file_new_content(self, mock_upload_logs):
+        """
+        An existing file with new content calls upload_logs and updates db size
+        """
+        file = pathlib.Path(f"{self.path_1}/{self.file_1_name}")
+        file.write_text(self.file_1_content * 2)
+        run.process_file(
+            directory=self.directory_1, file=file, session=self.session
+        )
+        self.assertTrue(mock_upload_logs.called)
+        db_file = self.session.query(model.File).first()
+        self.assertEqual(db_file.size, len(self.file_1_content) * 2)
+
+    @mock.patch("run.upload_logs")
+    def test_process_new_file(self, mock_upload_logs):
+        """A new file that isn't in the db is added and calls upload_logs"""
+        self.fs.create_file(f"{self.path_1}/new_fake_file")
+        new_fake_file = pathlib.Path(f"{self.path_1}/new_fake_file")
+        new_fake_file.write_text("testing")
+        run.process_file(
+            directory=self.directory_1,
+            file=new_fake_file,
+            session=self.session,
+        )
+        self.assertTrue(mock_upload_logs.called)
+        db_file = (
+            self.session.query(model.File)
+            .filter_by(name=new_fake_file.name)
+            .first()
+        )
+        self.assertEqual(db_file.size, new_fake_file.stat().st_size)
+
+    @unittest.skip("to be implemented")
+    def test_upload_logs(self):
+        pass
